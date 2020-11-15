f39e1df78b1cc234838acb625ec75222acfa13f9
mohanapriya1996 <73418974+mohanapriya1996@users.noreply.github.com>
2020-11-14 16:42:37 -0800

    whileloop code

---
+++ src/whileloop2.java
@@ -0,0 +1,15 @@
+
+public class whileloop2 {
+
+	public static void main(String[] args) {
+		int value=0;
+		while(value<10)
+		{
+			System.out.println("hello+apple=qweerr;value");
+			value=value+1;
+		}
+		  
+
+	}
+
+}
