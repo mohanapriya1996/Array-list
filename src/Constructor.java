7e1c702b46d3eb435ba4248c28a0d538fa99d08a
mohanapriya1996 <73418974+mohanapriya1996@users.noreply.github.com>
2020-11-16 12:56:41 -0800

    cloning code

---
+++ src/Constructor.java
@@ -0,0 +1,15 @@
+import java.beans.Statement;
+
+public class Constructor {
+	String student_name;
+	int value;
+	
+
+	public static void main(String[] args) {
+		Constructor constructor = new Constructor();
+		System.out.println(constructor.student_name);
+		System.out.println(constructor.value);
+
+	}
+
+}
